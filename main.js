ade6e8dea02a507c1a2e55fdd01cfa28acd9bd5e
Gabi Zanini <psicologia.plus@gmail.com>
2022-07-30 17:14:51 -0300

    Created and exported pwd dataBase file

---
+++ main.js
@@ -0,0 +1,8 @@
+import {dataBase} from "./pwdDataBase.js";
+const testBtn = document.getElementById("test-btn");
+const password = document.getElementById("password").innerText;
+
+
+testBtn.addEventListener("click", () => {
+
+})
